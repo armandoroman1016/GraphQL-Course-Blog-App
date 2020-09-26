262a8f634b5f1eced19e17622022c0fb9409cb94
Armando Roman <armandoroman1016@gmail.com>
2020-09-25 21:40:39 -0700

    subscriptions added for comments and posts

--- project/src/resolvers/Subscription.js
+++ project/src/resolvers/Subscription.js
@@ -12,6 +12,22 @@ const Subscription = {
 
             return pubsub.asyncIterator('count')
         }
+    },
+    comment: {
+        subscribe(parent, args, {db, pubsub}, info){
+            const {postID} = args
+
+            const post = db.posts.find(p => p.id === postID && p.published)
+
+            if(!post) throw new Error("Post not found")
+
+            return pubsub.asyncIterator(`comment ${postID}`)
+        }
+    },
+    post: {
+        subscribe(parent, args, {db, pubsub}, info){
+            return pubsub.asyncIterator("posts")
+        }
     }
 }
 
